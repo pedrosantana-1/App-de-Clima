0066ab057aba4c2e92c08318e5640bd414b58cba
Pedro Santana <1221pedrog@gmail.com>
2026-04-26 14:00:53 -0300

    Upgrade final

--- js/script.js
+++ js/script.js
@@ -1,22 +1,50 @@
 async function buscarClima() {
-            const cidade = document.getElementById("cidade").value;
-            const apiKey = "6f455bcd9fc628f63a741c79e975b4ce";
-            
+    const cidade = document.getElementById("cidade").value;
+    const apiKey = "6f455bcd9fc628f63a741c79e975b4ce";
 
-            const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt`;
+    if (!cidade) {
+        alert("Digite uma cidade");
+        return;
+    }
 
-            const resposta = await fetch(url);
-            const dados = await resposta.json();
+    try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt`;
 
-            if (dados.cod === "404") {
+        const resposta = await fetch(url);
+        const dados = await resposta.json();
+
+        // Erro de cidade não encontrada
+        if (dados.cod == 404) {
             alert("Cidade não encontrada");
             return;
-            }
+        }
 
-            console.log(dados);
+        // Preencher dados na tela
+        document.getElementById("city").innerText = dados.name;
+        document.getElementById("temp").innerText = Math.round(dados.main.temp) + "°C";
+        document.getElementById("desc").innerText = dados.weather[0].description;
+        document.getElementById("humidity").innerText = "Umidade: " + dados.main.humidity + "%";
 
-            document.getElementById("city").innerText = dados.name;
-            document.getElementById("temp").innerText = Math.round(dados.main.temp) + "°C";
-            document.getElementById("desc").innerText = dados.weather[0].description;
-            document.getElementById("humidity").innerText = "Umidade: " + dados.main.humidity + "%";
-            }
+        // Ícone do clima
+        const iconCode = dados.weather[0].icon;
+        document.getElementById("icon").src = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+    } catch (erro) {
+        console.error("Erro:", erro);
+        alert("Erro ao buscar dados");
+    }
+}
+
+
+// Evento de clique no botão
+document.getElementById("btnBuscar")
+    .addEventListener("click", buscarClima);
+
+
+// Buscar ao apertar ENTER
+document.getElementById("cidade")
+    .addEventListener("keypress", function (e) {
+        if (e.key === "Enter") {
+            buscarClima();
+        }
+    });
